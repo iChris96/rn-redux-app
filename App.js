2a36e7482a16028b2d42ab0f2bb3b3cf8c500657
christopher_windows <christopher_x10x@hotmail.com>
2021-04-06 22:19:16 -0500

    folder structure for store!

--- App.js
+++ App.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import * as reducers from './src/reducers';
-
-const store = createStore(combineReducers({
-  ...reducers
-}))
-
-const Home = () => {
-  console.log("store", store.getState())
-  return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      <Text>{store.getState().filters}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Home from './src';
+import store from './src/stores'
 
 export default function App() {
   return (
