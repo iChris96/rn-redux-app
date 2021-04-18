a1d87d6c3a373b84b2d8e00714afd9476453bf34
christopher_windows <christopher_x10x@hotmail.com>
2021-04-18 12:29:20 -0500

    install redux-thunk

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,5 +1,6 @@
 import { combineReducers, createStore } from 'redux';
 import * as reducers from '../reducers';
+import thunk from 'redux-thunk';
 
 export default createStore(combineReducers({
     ...reducers
